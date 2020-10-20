1ce8c605fdeb3be22363c5034ac3082457070cb3
djamal2727 <71788919+djamal2727@users.noreply.github.com>
2020-10-20 10:15:29 -0600

    Add files via upload

---
+++ Model Validation/setup.py
@@ -0,0 +1,43 @@
+# setup.py
+# only if building in place: ``python setup.py build_ext --inplace``
+
+import os
+import sys
+import platform
+import glob
+from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
+
+os.environ['NPY_DISTUTILS_APPEND_FLAGS'] = '1'
+
+#if os.name == 'nt':  # Windows.
+#    extra_compile_args = ['/TC', '/D', 'ANSI'] # for msvs
+#     # TODO: Not with Anaconda MINGW
+#else:
+#extra_compile_args = ''
+
+froot = 'pyframe3dd' + os.sep + 'src' + os.sep
+pyframeExt = Extension('pyframe3dd._pyframe3dd', sources=[froot+'py_HPGmatrix.c',
+                                               froot+'HPGutil.c',
+                                               froot+'NRutil.c',
+                                               froot+'coordtrans.c',
+                                               froot+'preframe.c',
+                                               froot+'py_eig.c',
+                                               froot+'py_frame3dd.c',
+                                               froot+'py_io.c',
+                                               froot+'py_main.c'])
+
+setup(
+    name='pyFrame3DD',
+    version='1.1.1',
+    description='Python bindings to Frame3DD',
+    author='NREL WISDEM Team',
+    author_email='systems.engineering@nrel.gov',
+    #package_dir={'': 'src'},
+    #py_modules=['pyframe3dd'],
+    package_data={'pyframe3dd': []},
+    packages=['pyframe3dd'],
+    license='Apache License, Version 2.0',
+    ext_modules=[pyframeExt],
+    zip_safe=False
+)
